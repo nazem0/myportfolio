f145087594b1c5a33b0c0c62eef12d778c97aee1
Nazem0 <Mohamednazem09@gmail.com>
2023-08-20 19:05:13 +0300

    added animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,34 @@
-// const skillsContainer = document.getElementById('skills');
 function consequentialShow() {
+  const head = document.getElementsByTagName("head")[0];
+  head.innerHTML += `
+  <style>
+      .pop{
+      animation: suddenShow 1s 1 ease-in-out;}
+      @keyframes suddenShow {
+          0%{scale: 0;}
+          100%{scale:1;}
+      }
+  </style>
+  `;
   divs = document.getElementsByClassName('consq');
   for (div of divs) {
-    let children = div.children;
-    for (let i = 0; i < children.length; i++) {
-      children[i].classList.add("d-none");
-      setTimeout(() => {
+      let children = div.children;
+      for (let i = 0; i < children.length; i++) {
+          let display = children[i].style.display;
+          console.log(display);
+          children[i].style.display = 'none';
+          setTimeout(() => {
 
-        children[i].classList.remove("d-none");
+              children[i].style.display = display;
 
-        children[i].classList.add("pop");
-      }, i * 400);
-    }
+              children[i].classList.add("pop");
+          }, i * 400);
+      }
   };
 
 }
-
-consequentialShow();
-// consequentialShow(document.getElementById('contact-buttons'));
+let waiting = new Promise(function (resolve, reject) {ScrollReveal().reveal('.scrollReveal', { scale: 1.2,rotate: {
+  x: -5,
+  z: -5
+},duration: 1000 }) });
+waiting.then(consequentialShow());
