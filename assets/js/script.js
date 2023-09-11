159bd821a26b8ff070076aed1d91bf0b34b8b6a7
nazem0 <mohamednazem09@gmail.com>
2023-09-11 21:21:29 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,24 @@
+import "./scrollreveal.js";
+
 function consequentialShow() {
-  const head = document.getElementsByTagName("head")[0];
-  head.innerHTML += `
-  <style>
-      .pop{
-      animation: suddenShow 1s 1 ease-in-out;}
-      @keyframes suddenShow {
-          0%{scale: 0;}
-          100%{scale:1;}
-      }
-  </style>
-  `;
-  divs = document.getElementsByClassName('consq');
-  for (div of divs) {
-      let children = div.children;
-      for (let i = 0; i < children.length; i++) {
-          let display = children[i].style.display;
-          console.log(display);
-          children[i].style.opacity = 0;
-          setTimeout(() => {
-
-            children[i].style.opacity = 1;
-
-              children[i].classList.add("pop");
-          }, i * 200);
-      }
-  };
+    let divs = document.getElementsByClassName('consq');
+    for (let div of divs) {
+        let children = div.children;
+        for (let i = 0; i < children.length; i++) {
+            children[i].style.opacity=0;
+            setTimeout(() => {
+                children[i].classList.add("pop");
+                children[i].removeAttribute('style')
+            }, i * 350);
+        }
+    };
 
 }
-let waiting = new Promise(function (resolve, reject) {ScrollReveal().reveal('.scrollReveal', { scale: 1.2,rotate: {
-  x: 20,
-  z: 3
-},duration: 1000 }) });
-waiting.then(consequentialShow());
+
+await ScrollReveal().reveal('.scrollReveal', {
+    scale: 0.4,
+    distance: '-200px'
+    , duration: 1500
+})
+
+consequentialShow()
